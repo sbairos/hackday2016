f56d206583ea2ca467bc2a347e1c0df020259d30
Steven Bairos <sbairosnovak@gmail.com>
2016-12-03 12:14:39 -0800

    Adding a header, more mock entries

--- src/db/MockDB.js
+++ src/db/MockDB.js
@@ -15,7 +15,26 @@ import Article from '../obj/Article'
 var mock;
 
 function MockDB() {
-	mock = [new Article('name1', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)')];
+	mock = [new Article('name1', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name2', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name3', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name4', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name5', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name6', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name7', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name8', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name9', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name10', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name11', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name12', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name13', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name14', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name15', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name16', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name17', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name18', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	new Article('name19', 'comment1', 'https://en.wikipedia.org/wiki/Halsey_(singer)'),
+	];
 }
 
 MockDB.prototype = new MockDB();
