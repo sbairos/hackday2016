5f1913a1a3e45e6a74be195300d28eb76da84db9
Steven Bairos <sbairosnovak@gmail.com>
2016-12-03 19:08:45 -0800

    Really? That was the bug? REALLLLYYYYY? PJSalt

--- src/ReadQueue.js
+++ src/ReadQueue.js
@@ -16,7 +16,39 @@ class ReadQueue extends React.Component {
       rq = [];
     }
     this.onRemove = this.onRemove.bind(this);
-    this.state = {username: username, items: rq};
+    console.log(this);
+    this.handleChangeN = this.handleChangeN.bind(this);
+    this.handleChangeC = this.handleChangeC.bind(this);
+    this.handleChangeU = this.handleChangeU.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      username: username, 
+      items: rq,
+      addN: '',
+      addC: '',
+      addU: '',
+    };
+  }
+
+  handleChangeN(event) {
+    this.setState({addN: event.target.value});
+  }
+
+  handleChangeC(event) {
+    this.setState({addC: event.target.value});
+  }
+
+  handleChangeU(event) {
+    this.setState({addU: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+    var list = this.state.items;
+    list.push(new Article(this.state.addN, this.state.addC, this.state.addU));
+    this.setState({items: list, addN:'', addC:'', addU:''});
   }
 
   onRemove(event) {
@@ -30,19 +62,31 @@ class ReadQueue extends React.Component {
   }
 
   render() {
-    var onRemove = this.onRemove
+    var onRemove = this.onRemove;
+    var handleChangeN = this.handleChangeN;
+    var handleChangeC = this.handleChangeC;
+    var handleChangeU = this.handleChangeU;
+    var handleSubmit = this.handleSubmit;
     return (
-      <ol>
-        {this.state.items.map(function(article){
-          return (
-            <li key={article.name}>
-              <a href={article.url}>{article.name}</a> - {article.comment}
-              <button id={article.name} className='glyphicon glyphicon-remove' onClick={onRemove}></button>
-            </li>
-          );
-        })}
-      </ol>
-      );
+      <div>
+        <form className="navbar-form navbar-left" onSubmit={handleSubmit}>
+          <input type="text" className="form-control" placeholder="Article Name" value={this.state.addN} onChange={handleChangeN} />
+          <input type="text" className="form-control" placeholder="Comment" value={this.state.addC} onChange={this.handleChangeC} />
+          <input type="text" className="form-control" placeholder="URL" value={this.state.addU} onChange={this.handleChangeU} />
+          <input type="submit" className="btn btn-default" value = "Submit" />
+        </form>
+        <ol>
+          {this.state.items.map(function(article){
+            return (
+              <li key={article.name}>
+                <a href={article.url}>{article.name}</a> - {article.comment}
+                <button id={article.name} className='glyphicon glyphicon-remove' onClick={onRemove}></button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    );
   }
 }
 export default ReadQueue
