7856718da3b8238b30bd45ab1a3d97a12e74ab36
Steven Bairos <sbairosnovak@gmail.com>
2016-12-03 18:23:57 -0800

    Can almost delete

--- src/ReadQueue.js
+++ src/ReadQueue.js
@@ -5,26 +5,38 @@ var MockDB = require('./db/MockDB');
 var db = new MockDB();
 // console.log(db.getArticles(localStorage.get('username')));
 class ReadQueue extends React.Component {
-    constructor() {
-        super();
-        this.username = '';
-        if (localStorage.username) {
-            this.username = localStorage.username;
-        }
-        this.rq = db.getArticles(this.username);
+  constructor() {
+    super();
+    this.username = '';
+    if (localStorage.username) {
+      this.username = localStorage.username;
     }
+    this.rq = db.getArticles(this.username);
+    this.onRemove = this.onRemove.bind(this);
+  }
 
-    render() {
+  onRemove(event) {
+    console.log(event.target);
+    var i =0;
+    for (i = 0; i < this.rq.length && event.target.id != this.rq[i].name; i++) {}
 
-        return (
-            // <div className='hi'> hello
-            // </div>
-            <ol>
-                {this.rq.map(function(article){
-                    return <li key={article.name}><a href={article.url}>{article.name}</a></li>;
-                })}
-            </ol>
-            );
-    }
+    this.rq.splice(i, 1);
+  }
+
+  render() {
+    var onRemove = this.onRemove
+    return (
+      <ol>
+        {this.rq.map(function(article){
+          return (
+            <li key={article.name}>
+              <a href={article.url}>{article.name}</a> - {article.comment}
+              <button id={article.name} className='glyphicon glyphicon-remove' onClick={onRemove}></button>
+            </li>
+          );
+        })}
+      </ol>
+      );
+  }
 }
 export default ReadQueue
